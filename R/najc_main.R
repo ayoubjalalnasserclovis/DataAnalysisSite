5b595eaec66cc4190a9be8075921ff28378b6706
ayoubjalalnasserclovis <naji.ayoub2213@gmail.com>
2024-12-31 17:37:55 +0100

    Update and rename xxxx_main.R to najc_main.R

---
+++ R/najc_main.R
@@ -0,0 +1,14 @@
+
+
+source("najc_src_utilities.R")
+
+# Run patent data processing (base_brevets)
+source("najc_patent.R")
+
+# Run job data processing (base_emp)
+source("najc_job.R")
+
+# Match databases (base_emp_inno)
+source("najc_match.R")
+
+
